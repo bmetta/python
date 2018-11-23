27b8698d282769756ff9d67b4aa45764c5b19bc9
bmetta <bhogeswar.m@gmail.com>
2018-11-23 17:48:54 +0530

    regex changes for finditer and search

--- regex/findIter.py
+++ regex/findIter.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 import re
 
-#str = 'rabcdeefgyYhFjkIoomnpOeorteeeeet'
-str = 'abaabaabaabaaefEAEOUOIouf'
+str = 'rabcdeefgyYhFjkIoomnpOeorteeeeet'
+# rabcdeefgyYhFjkIoomnpOeorteeeeet
+# 012345678901234567890123456789012
+
+#str = 'abaabaabaabaaefEAEOUOIouf'
+# abaabaabaabaaefEAEOUOIouf
+# 01234567890123456789012345
 for match in re.finditer(r'[^aeiou]{1}[aeiou]{2,}[^aeiou]{1}', str, flags = re.I):
   s = match.start()
   e = match.end()
-  print s, e
-  print str[s+1:e-1]
+  print s, e, str[s:e]
--- regex/search.py
+++ regex/search.py
@@ -3,20 +3,19 @@ import re
 
 match = re.search(r'[\w.]+@[\w.]+', 'blah nick.p@gmail.com blah blah')
 if match:
-  print 'match.groups()', match.groups()
-  print 'match.group() ', match.group()
-  print 'match.group(0)', match.group(0)
+  print 'match.groups()', match.groups()    # ()
+  print 'match.group() ', match.group()     # nick.p@gmail.com
+  print 'match.group(0)', match.group(0)    # nick.p@gmail.com
 else:
   print 'Not found'
-
 print '--------------------------'
 
 match = re.search(r'([\w.]+)@([\w.]+)', 'blah nick.p@gmail.com blah blah')
 if match:
-  print 'match.groups()', match.groups()
-  print 'match.group() ', match.group()
-  print 'match.group(0)', match.group(0)
-  print 'match.group(1)', match.group(1)
-  print 'match.group(2)', match.group(2)
+  print 'match.groups()', match.groups()    # ('nick.p', 'gmail.com')
+  print 'match.group() ', match.group()     # nick.p@gmail.com
+  print 'match.group(0)', match.group(0)    # nick.p@gmail.com
+  print 'match.group(1)', match.group(1)    # nick.p
+  print 'match.group(2)', match.group(2)    # gmail.com
 else:
   print 'Not found'
